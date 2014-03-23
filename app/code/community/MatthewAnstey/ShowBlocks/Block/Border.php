fde67214da7fb22280405da7f8fa0c963eb0e6ca
= <mtthwnsty@gmail.com>
2014-03-23 13:28:08 +0000

    Renamed Company name and updated class check to something more efficient.

---
+++ app/code/community/MatthewAnstey/ShowBlocks/Block/Border.php
@@ -0,0 +1,32 @@
+
+<?php
+
+class MatthewAnstey_ShowBlocks_Block_Border extends Mage_Cms_Block_Block
+{
+    /**
+     * Prepare Content HTML
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        $blockId = $this->getBlockId();
+        $blockName = $this->getBlockAlias();
+        $html = '';
+        if ($blockId) {
+            $block = Mage::getModel('cms/block')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($blockId);
+            if ($block->getIsActive()) {
+                /* @var $helper Mage_Cms_Helper_Data */
+                $helper = Mage::helper('cms');
+                $processor = $helper->getBlockTemplateProcessor();
+                $html = $processor->filter($block->getContent());
+                $this->addModelTags($block);
+            }
+        }
+         
+        return '<div class="cms-block-wrapper"><span class="cms-block-title">' . $blockName . '</span><div class="cms-block">' . $html . '</div></div>';
+    }
+}
+
--- app/code/community/MatthewAnstey/ShowBlocks/Model/Observer.php
+++ app/code/community/MatthewAnstey/ShowBlocks/Model/Observer.php
@@ -9,7 +9,7 @@ class VisibleBlocks_ShowBlocks_Model_Observer
         $block = $observer->getEvent()->getBlock();
         $blockName = $block->getBlockAlias();
         
-         if(get_class($block) == "Mage_Cms_Block_Block" || is_subclass_of($block, "Mage_Cms_Block_Block")){
+         if($block instanceof Mage_Cms_Block_Block){
              $transport->setHtml('<div class="cms-block-wrapper"><span class="cms-block-title">' . $blockName . '</span><div class="cms-block">' . $html . '</div></div>');
          }
         
