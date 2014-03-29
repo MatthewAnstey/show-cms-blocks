a610af9ad026dbd66fa3764d8ada7d06b0e6a458
= <mtthwnsty@gmail.com>
2014-03-29 23:33:38 +0000

    Added admin configuration

--- app/code/community/MatthewAnstey/ShowBlocks/Model/Observer.php
+++ app/code/community/MatthewAnstey/ShowBlocks/Model/Observer.php
@@ -4,15 +4,20 @@ class MatthewAnstey_ShowBlocks_Model_Observer
 {
     public function wrapHtml($observer)
     {
-        if(Mage::helper('showblocks')->isUserAdmin()){
+        $enabled = Mage::getStoreConfig('matthewanstey/matthewanstey_group/matthewanstey_select',Mage::app()->getStore());
+    
+        if(Mage::helper('showblocks')->isUserAdmin() && $enabled){
+            
             $transport = $observer->getEvent()->getTransport();
             $html = $transport->getHtml();
             $block = $observer->getEvent()->getBlock();
             $blockName = $block->getBlockAlias();
-            if($block instanceof Mage_Cms_Block_Block){
-                $transport->setHtml('<div class="cms-block-wrapper"><span class="cms-block-title">' . $blockName . '</span><div class="cms-block">' . $html . '</div></div>');
-            }
-        } 
+        
+         if($block instanceof Mage_Cms_Block_Block){
+            $transport->setHtml('<div class="cms-block-wrapper"><span class="cms-block-title">' . $blockName . '</span><div class="cms-block">' . $html . '</div></div>');
+         }
+         
+        }  
     }
     
 }
